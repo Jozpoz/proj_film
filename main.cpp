4c728c5d7daf44d4d3eddf3c663c5855dc611ef1
Jozpoz <joz.poz@gmail.com>
2016-06-08 08:37:39 +0200

    Project FILM ver. 0.2.2
    Changed layout of the film to the working one.

--- main.cpp
+++ main.cpp
@@ -2,92 +2,100 @@
 #include "actor.h"
 #include "film.h"
 #include <iostream>
-#include <stdio.h>
-#include <time.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <vector>
+#include "main.h"
+//#include <stdio.h>
+//#include <time.h>
+//#include <iterator>
+//#include <unistd.h>
+//#include <stdlib.h>
+//#include <vector>
 
 #define AMOUNT_OF_DIRECTORS 20
-#define AMOUNF_OF_ACTORS 120
+#define AMOUNT_OF_ACTORS 120
 #define WEEKS 52
 #define YEARS 20
 
+void weekFight();
+void boxOfficeResults();
+void boxOfficeResults();
 
-Film filmVector[2*WEEKS];
-Film filmWeeklyWinnerBoxOffice[WEEKS];
+void ratingResults();
+
+Film filmVector[2 * WEEKS];
 Film filmWeeklyWinnerRating[WEEKS];
+Film filmWeeklyWinnerBoxOffice[WEEKS];
+
+int main (int argc, char const *argv[])
+{
+	Director poolOfDirectors[AMOUNT_OF_DIRECTORS];
+	Actor poolOfActors[AMOUNT_OF_ACTORS];
+	Film::assignCast( poolOfActors );
+
+	weekFight();
+	boxOfficeResults();
+	ratingResults();
+}
 
 void weekFight()
 {
-	for (int k = 0; k < WEEKS; k++)
-		cout << "Tydzien #" << k << ": '" << filmVector[k]._title << endl << "(dir. " << filmVector[k]._director.name << ")" << endl << "(starring: " << filmVector[k].cast[0] << ", " << filmVector[k].cast[1] << ", " << filmVector[k].cast[2] << ", " << filmVector[k].cast[3] << ", " << filmVector[k].cast[4] << ", " << filmVector[k].cast[5] << ")"<< endl;
+
+	int k = 0;
+	int l = 0;
+	int m = 0;
+	while (k < WEEKS)
+	{
+		int i = 0;
+		int j = 1;
+
+		//Zaprezentowanie filmów:
+
+		std::cout << "Week #" << k << ": " << endl;
+		std::cout << "'" << filmVector[i].title() << "'" << endl << "(dir. " << filmVector[i].director().name() << ")" << endl << "(starring: " << filmVector[i].cast[0][0].name() << ", " << filmVector[i].cast[1][0].name() << ", " << filmVector[i].cast[2][0].name() << ", " << filmVector[i].cast[3][0].name() << ", " << filmVector[i].cast[4][0].name() << ", " << filmVector[i].cast[5][0].name() << ")" << endl;
+		std::cout << endl << "vs" << endl;
+		std::cout << "'" << filmVector[j].title() << "'" << endl << "(dir. " << filmVector[j].director().name() << ")" << endl << "(starring: " << filmVector[j].cast[0][0].name() << ", " << filmVector[j].cast[1][0].name() << ", " << filmVector[j].cast[2][0].name() << ", " << filmVector[j].cast[3][0].name() << ", " << filmVector[j].cast[4][0].name() << ", " << filmVector[j].cast[5][0].name() << ")" << endl;
+
+		//Porównanie box office
+
+		if (filmVector[i].box_office() < filmVector[j].box_office())
+		{
+			filmWeeklyWinnerBoxOffice[k] = filmVector[i];
+		}
+		else
+		{
+			filmWeeklyWinnerBoxOffice[k] = filmVector[j];
+		}
+
+		//Porownanie rating
+
+		if (filmVector[i].rating() < filmVector[j].rating())
+		{
+			filmWeeklyWinnerRating[k] = filmVector[i];
+		}
+		else
+		{
+			filmWeeklyWinnerRating[k] = filmVector[j];
+		}
+		i=i+2;
+		j=j+2;
+		k++;
+
+	}
 }
-
-
-
-void weekBoxOffice()
+void boxOfficeResults()
 {
 	int k = 0;
-	while(k < WEEKS)
-		{
-			int i = 0;
-			int j = 1;
-			
-
-		if (filmVector._box_office[i] < filmVector._box_office[j]) 
-			filmWeeklyWinnerBoxOffice[k] = filmVector[i]
-		else 
-			filmWeeklyWinnerBoxOffice[k] = filmVector[j]
-
-			i+2;
-			j+2;
-			k++;
-		 
-		}
+	std::cout << "Best-selling films in each week: " << endl;
+	while (k < WEEKS)
+	{
+		std::cout << "Week #" << k << ": '" << filmWeeklyWinnerBoxOffice[k].title() << "': $ " << filmWeeklyWinnerBoxOffice[k].box_office() << endl;
+	}
 }
-
-void weekRating()
+void ratingResults()
 {
 	int k = 0;
-	while(k < WEEKS)
-		{
-			int i = 0;
-			int j = 1;
-			
-
-		if (filmVector._rating[i] < filmVector._rating[j]) 
-			filmWeeklyWinnerBoxOffice[k] = filmVector[i]
-		else 
-			filmWeeklyWinnerBoxOffice[k] = filmVector[j]
-
-			i+2;
-			j+2;
-			k++;
-		 
-		}
-}
-
-void showWeeks()
-{
-	cout << "Najlepiej zarabiajace filmy w kolejnych tygodniach:"<< endl;
-	for (int k = 0; k < WEEKS; k++)
+	std::cout << "Top rated films in each week: " << endl;
+	while (k < WEEKS)
 	{
-		cout << "Tydzien #" << k << ": " << filmWeeklyWinnerBoxOffice[k]._title << " zarobil: $" << filmWeeklyWinnerBoxOffice[k]._box_office << endl; 
+		std::cout << "Week #" << k << ": '" << filmWeeklyWinnerRating[k].title() << "': " << filmWeeklyWinnerRating[k].rating() << " / 10" << endl;
 	}
-
-	cout << "Najlepiej oceniane filmy w kolejnych tygodniach:"<< endl;
-	for (int k = 0; k < WEEKS; k++)
-	{
-		cout << "Tydzien #" << k << ": " << filmWeeklyWinnerRating[k]._title << " zarobil: $" << filmWeeklyWinnerRating[k]._box_office << endl; 
-	}
-
-}
-
-int main (int argc, char constant argv[])
-{
-	Dir poolOfDirectors[AMOUNT_OF_DIRECTORS];
-	Act poolOfActors[AMOUNF_OF_ACTORS];
-
-	Film::assignCast( &poolOfActors )
 }
